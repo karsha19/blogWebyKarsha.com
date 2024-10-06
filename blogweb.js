0ae8978dad9dc69967e73350f8a5ec6a4fff5e71
Raksha <rakshaadhikari697@gmail.com>
2024-10-06 13:19:04 +0530

    yes

--- blogweb.js
+++ blogweb.js
@@ -3,6 +3,3 @@ console.log("raksha adhikari");
 console.log("yes");
 console.log("yeyyyy! i did it and i love solving problems!!!!!!!!");
 console.log("i solved this problem again!!");
-let a=prompt("enter the a: ");
-console.log(a+a);
-console.log("yes");
